49ca13cf38227bf83a4696fb1c7f159f86bba732
calculus guy <sakariyauabdullateef993@gmail.com>
2026-03-10 22:54:56 +0100

    chore : clock In V4

--- src/SEO.js
+++ src/SEO.js
@@ -25,7 +25,7 @@ const SEO = ({ title, description, keywords, url, image }) => {
       <meta name="twitter:image" content={image || "https://olawoyin.netlify.app/twitter-image.jpg"} />
 
       {/* Canonical URL (SEO Best Practice) */}
-      <link rel="canonical" href={url} />   
+      <link rel="canonical" href={url} />    
     </Helmet>
   );
 };
